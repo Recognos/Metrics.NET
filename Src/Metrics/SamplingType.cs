fd3ff21ae8460ecb2d5896d156d70d13acff1b12
Alistair Clark <alistairclark7@gmail.com>
2016-04-18 09:19:47 +0100

    Corrected typos in SamplingType comments

--- Src/Metrics/SamplingType.cs
+++ Src/Metrics/SamplingType.cs
@@ -16,7 +16,7 @@ namespace Metrics
         FavourRecent,
 
         /// <summary>
-        /// Sampling will done with a Uniform Reservoir.
+        /// Sampling will be done with a Uniform Reservoir.
         /// </summary>
         /// <remarks>
         /// A histogram with a uniform reservoir produces quantiles which are valid for the entirely of the histogram’s lifetime.
@@ -28,7 +28,7 @@ namespace Metrics
         LongTerm,
 
         /// <summary>
-        /// Sampling will done with a Sliding Window Reservoir.
+        /// Sampling will be done with a Sliding Window Reservoir.
         /// A histogram with a sliding window reservoir produces quantiles which are representative of the past N measurements.
         /// More information about <a href="http://metrics.codahale.com/manual/core/#man-core-histograms">Exponentially Decaying Reservoir</a>
         /// </summary>
